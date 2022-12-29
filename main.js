26fece67486790be96da626912cffb529c12d890
ChristoferRian <christoferrian@gmail.com>
2022-12-29 20:09:00 +0700

    commit ke-2

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-let todoName = document.getElementById("todo-name"); //mengambil element berupa id, element todo-name
-let todoSimpan = document.getElementById("todo-simpan"); //mengambil element berupa id, element todo-simpan
+let todoName = document.getElementById("todo-name"); //mengambil element berupa id, element todo-name (di line 22 html)
+let todoSimpan = document.getElementById("todo-simpan"); //mengambil element berupa id, element todo-simpan (di line 29 html)
 
 todoSimpan.addEventListener("click", function () {
+  //membuat button simpan bisa menerima kegiatan yang di input dengan menggunakan addEventListener dan yang mentrigger adalah click, lalu setelah di 'click' button nya maka akan menjalankan sebuah function
   if (todoName.value == "") {
-    alert("silahkan input kegiatan");
+    alert("silahkan input kegiatan"); //kalau di kolom input gak ada apa-apa atau NULL, maka akan diberikan allert
   } else {
-    let todoContainer = document.querySelector(".list-group");
-    let todoHTML = todoContainer.innerHTML;
-    todoHTML += `
+    let todoContainer = document.querySelector(".list-group"); //membuat sebuah variabel bernama todoContainer yang isi nya adalah mencari element yang bernama list-group (tanda . atau 'titik' itu wajib dipakai kalau mau memasukkan element yang lain)
+    let todoHTML = todoContainer.innerHTML; //lalu membuat variabel baru bernama todoHTML yang isi nya adalah mengubah property "list-group" (line 35) dengan parameter yang akan diberikan berikutnya dengan menggunakan .innerHTML
+    todoHTML += ` 
           <li class="list-group-item d-flex justify-content-between">
             <div>
               <input class="form-check-input me-1" type="checkbox">
               <span>${todoName.value}</span>
             </div>
             <button class="badge border-0 bg-danger btn-hapus">x</button>
-          </li>`;
-    todoContainer.innerHTML = todoHTML;
-    todoName.value = "";
+          </li>`; //ini merupakan parameter yang akan masukkan ke property "list-group" dan btw itu yang setelah += kan ada tanda kecil, nah itu namanya 'backtick' dan ada di bawah tombol esc keyboard
+    todoContainer.innerHTML = todoHTML; //dan ini bisa dikatakan kalau kita mengulangi proses yang sama kayak tadi, kalau gak ngerti ini sama aja kayak kita kembali ke line 10 diatas
+    todoName.value = ""; //ini bikin form input nya jadi bersih kembali
     todoName.focus();
 
-    let checkTodo = document.querySelectorAll(".form-check-input");
+    let checkTodo = document.querySelectorAll(".form-check-input"); //kita membuat variabel yang akan membuat list nya
     for (let i = 0; i < checkTodo.length; i++) {
       const input = checkTodo[i];
       input.addEventListener("change", function () {
@@ -36,6 +37,5 @@ todoSimpan.addEventListener("click", function () {
       });
     }
   }
-}); //membuat button simpan bisa menerima kegiatan yang di input dengan menggunakan addEventListener dan yang mentrigger adalah click, lalu setelah di 'click' button nya maka akan menjalankan function di
-
+});
 console.log(todoName);
